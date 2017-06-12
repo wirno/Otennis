5c9d2052463a4c1810f37f1d71fe33cb171e9758
wirno <wirno@hotmail.fr>
2017-06-12 08:05:39 +0200

    add indexer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-devise_for :users, controllers: { registrations: 'users/registrations' }
+	devise_for :users, controllers: { registrations: 'users/registrations' }
 
 	root to: 'home#index'
 	get 'home', to:'home#index', as: 'home'
+
+	get 'indexer/index'
+  	get 'indexer/reindex'
 end
