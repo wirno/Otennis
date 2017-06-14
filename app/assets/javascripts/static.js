b99bc98eb0bffc93a93012314395d701693a02e3
math1761 <mathieulevi@gmail.com>
2017-06-14 19:38:25 +0200

    Finished integration of search and fixed login

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,12 +1,19 @@
-/*
 $(function () {
     $( "#slider-range" ).slider({
         range: true,
-        min: 0,
-        max: 500,
+        min: 1,
+        max: 100,
         values: [ 75, 300 ],
         slide: function( event, ui ) {
             $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
         }
     });
-});*/
+    $('#slider-age').slider({
+        range: true,
+        min: 18,
+        max: 75,
+        slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+    })
+});
